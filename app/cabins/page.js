172fbb64734120027ca507bb1c79c74803a0ae20
olah <olahisrael07@gmail.com>
2024-11-28 22:47:29 +0100

    sample lessons

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,9 +1,15 @@
-import Navigation from "../components/Navigation";
+export default async function Page() {
 
-export default function Page() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
     return (
         <div>
             <h1> Cabins page</h1>
+            <ul>{data.map((user) => (
+                <li key={user.id}>{user.name}</li>
+            ))}
+            </ul>
         </div>
     )
 }
