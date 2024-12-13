3edc376a3fb66e81794caf7d52dbbc6c250a5ce3
olah <olahisrael07@gmail.com>
2024-12-13 13:46:49 +0100

    Added dynamic route

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -11,8 +11,6 @@ export async function getCabin(id) {
     .eq('id', id)
     .single();
 
-  // For testing
-  // await new Promise((res) => setTimeout(res, 1000));
 
   if (error) {
     console.error(error);
@@ -40,6 +38,9 @@ export async function getCabins() {
     .from('cabins')
     .select('id, name, maxCapacity, regularPrice, discount, image')
     .order('name');
+
+    // For testing
+  // await new Promise((res) => setTimeout(res, 2000));
 
   if (error) {
     console.error(error);
--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,12 @@
-import CabinCard from "../_components/CabinCard";
-import { getCabins } from "../_lib/data-service";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
     title: "Cabins",
 };
 
-export default async function Page() {
-  console.log("starting...")
-    const cabins = await getCabins()
-    console.log(cabins)
+export default function Page() {
   
     return (
       <div>
@@ -23,14 +21,11 @@ export default async function Page() {
           away from home. The perfect spot for a peaceful, calm vacation. Welcome
           to paradise.
         </p>
-  
-        {cabins.length > 0 && (
-          <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-            {cabins.map((cabin) => (
-              <CabinCard cabin={cabin} key={cabin.id} />
-            ))}
-          </div>
-        )}
+
+        <Suspense fallback={<Spinner />}>
+          <CabinList />
+        </Suspense>
+          
       </div>
     );
   }
