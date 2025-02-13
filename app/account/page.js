fc9a9ebba0e5ed3110391bd54283ec1e5306e117
olah <olahisrael07@gmail.com>
2025-02-13 21:39:26 +0100

    implemented signout and signin feature

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,17 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
-    title: "Account",
+  title: "Guest area",
 };
 
-export default function Page() {
-    return (
-        <h2 className="font-semibold text-2xl text-colors-accent-400 mb-7">
-            Welcome, Olah
-        </h2>
-    )
+export default async function Page() {
+  const session = await auth();
+
+  const firstName = session.user.name.split(" ").at(0);
+
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {firstName}
+    </h2>
+  );
 }
